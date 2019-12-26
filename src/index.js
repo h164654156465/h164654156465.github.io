cdd58183d76d6fa00190e043af37b2712d49fac8
Mnemo7 <h164654156465@gmail.com>
2019-12-26 13:56:56 +0800

    Solve responsive layouts on mobile, fixed position header and centering

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ window.onscroll = function () {
             $("#header").css("background", "transparent");
             $("#header").css("color", "#fff");
             $("#header").css("box-shadow", "0px 0px 0px rgba(0,0,0,0)");
-            $("#header").css("padding", "6vh 4vw");
+            $("#header").css("padding", "4vh 4vw");
             $("#navigation a").hover(function () {
                 $(this).css("border-bottom", "2px solid #fff");
             }, function () {
